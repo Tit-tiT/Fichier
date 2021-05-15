90a84aed031546411379a15aac6c887a8f7661b1
titouan <bahon.titouan@gmail.com>
2021-05-15 17:58:17 +0200

    add Javadoc

--- Fichier.java
+++ Fichier.java
@@ -5,29 +5,48 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class Java permettant de Creer/Modifier/Lire/Supprimmer des fichiers en java.
+ */
 public class Fichier{
 
     private String nom;
     private String repertoire;
 
+    /**
+     * Constructeur de fichier.
+     * @param nom nom du fichier.
+     * @param repertoire répertoire ou sera positionné le fichier.
+     */
     public Fichier(String nom,String repertoire ){
         this.nom=nom;
         this.repertoire=repertoire;
     }
 
+    /**
+     * Constructeur de fichier avec comme répertoire : ".".
+     * @param nom nom du fichier.
+     */
     public Fichier(String nom){
         this(nom,".");
     }
 
 
+    /**
+     * Créer le fichier.
+     */
     public void cree(){
         try {
             File myObj = new File(this.getRepertoire()+"/"+this.getNom());
-            if (myObj.createNewFile()) {}
-            else System.out.println("Fichier deja existant");
+            if (!myObj.createNewFile()) System.out.println("Fichier deja existant");
         } catch (IOException e) {e.printStackTrace();}
     }
 
+    /**
+     * Écrit sur le fichier à la ligne donnée en parametre.
+     * @param data donnée à écrire dans le fichier.
+     * @param ligne ligne où écrire dans le fichier.
+     */
     public void ecrire(String data,int ligne){
         try {
             String[] tab = this.toTab();
@@ -67,6 +86,9 @@ public class Fichier{
         } catch (IOException e) {e.printStackTrace();}
     }
 
+    /**
+     * Lis le fichier en l'affichant dans la console.
+     */
     public void lire(){
         try {
             File myObj = new File(this.getRepertoire()+"/"+this.getNom());
@@ -83,6 +105,10 @@ public class Fichier{
         }
     }
 
+    /**
+     * Transforme le fichier en tableau de String.
+     * @return un tableau de String contenant tout le fichier ligne par ligne.
+     */
     public String[] toTab(){
         ArrayList<String> liste = new ArrayList<>();
         try {
@@ -100,16 +126,26 @@ public class Fichier{
         return liste.toArray(new String[liste.size()]);
     }
 
+    /**
+     * Supprime le fichier.
+     */
     public void supprimer(){
         File myObj = new File(this.getRepertoire()+"/"+this.getNom());
-        if (myObj.delete()) {}
-        else {System.out.println("Fichier impossible a supprimer.");}
+        if (!myObj.delete()) System.out.println("Fichier impossible a supprimer.");
     }
 
+    /**
+     * Retorune le nom du fichier.
+     * @return le nom du fichier.
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Retorune le répertoire du fichier.
+     * @return le répertoire du fichier.
+     */
     public String getRepertoire() {
         return repertoire;
     }
